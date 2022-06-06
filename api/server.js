86ce5f7bd96e26d05a86f41d6f8b65d29c22945f
Gage Clancy <gclancy121@gmail.com>
2022-06-06 16:49:10 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,68 @@
 // BUILD YOUR SERVER HERE
+// imports
+const express = require('express');
+const Users = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+//create server
+const server = express();
+
+// parse JSON payloads
+server.use(express.json());
+
+// SERVERS PARTS
+
+// Fetch all users
+server.get("/api/users", (req, res) => {
+  Users.find().then(result => {
+    res.json(result);
+  })
+})
+// Post new user
+server.post("/api/users", (req, res) => {
+  Users.insert(req.body).then(result => {
+    res.status(201).json(result);
+  })
+  
+})
+
+//Fetch a user by ID
+server.get("/api/users/:id", (req, res) => {
+  Users.findById(req.params.id).then(result => {
+    if (result == null) {
+      res.status(404).json({message: "user not found"});
+      return;
+    }
+    res.json(result);
+  })
+})
+
+// Delete a user
+server.delete('/api/users/:id', (req, res) => {
+
+  Users.remove(req.params.id).then(result => {
+    if (result == null) {
+      res.status(404).json({message: "user not found"});
+      return;
+    }
+    res.json(result);
+  });
+});
+
+// Update a user
+server.put('/api/users/:id', (req, res) => {
+  console.log(req.body);
+  Users.update(req.params.id, req.body).then(result => {
+    if (result == null) {
+      res.status(404).json({message: "user not found"});
+      return;
+    }
+    res.json(result);
+  })
+  .catch(err => {
+    res.status(500).json({message: err.message})
+  })
+});
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
